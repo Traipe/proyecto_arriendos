519ab7904713ac8ff18e45c14fed2f33e96dfe4d
traipe <jimenatraipe@gmail.com>
2024-07-24 12:29:15 -0400

    hito1

---
+++ arriendos/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+class Usuario(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    rut = models.CharField(max_length=12)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField()
+
+TIPO_USUARIO = [
+    ('arrendatario', 'Arrendatario'),
+    ('arrendador', 'Arrendador'),
+]
+
+
+tipo_usuario = models.CharField(max_length=12,
+    choices=TIPO_USUARIO)
+
+class Propiedad(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    m2_construidos = models.FloatField()
+    m2_totales = models.FloatField()
+    estacionamientos = models.IntegerField()
+    habitaciones = models.IntegerField()
+    banos = models.IntegerField()
+    direccion = models.CharField(max_length=200)
+    comuna = models.CharField(max_length=100)
+    
+TIPO_INMUEBLE = [
+    ('casa', 'Casa'),
+    ('departamento', 'Departamento'),
+    ('parcela', 'Parcela'),
+]
+
+tipo_inmueble = models.CharField(max_length=12,
+    choices=TIPO_INMUEBLE)
+precio_arriendo = models.FloatField()
+arrendador = models.ForeignKey(Usuario,
+    on_delete=models.CASCADE)
+
